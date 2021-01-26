499b1459dd223990c50fb4cba99430a259b89489
Ben8842 <z87e99b44r55a@gmail.com>
2021-01-25 23:06:20 -0500

    Q

--- src/App.js
+++ src/App.js
@@ -2,24 +2,101 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Building extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfo: false,
+      xCoor: 0,
+      yCoor: 0,
+      isChess: false,
+      size: 0,
+      chessCodeLetter: "",
+      chessCodeNumber: "",
+    };
+  }
+
+  showCode(x, y, sizes) {
+    this.setState((state) => {
+      if (sizes == 8) {
+        var chessArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
+        var chessX = chessArray[x];
+        var chessY = y + 1;
+
+        return {
+          showInfo: true,
+          xCoor: x,
+          yCoor: y,
+          isChess: true,
+          chessCodeLetter: chessX,
+          chessCodeNumber: chessY,
+        };
+      } else {
+        return { showInfo: true, xCoor: x, yCoor: y, isChess: false };
+      }
+    });
+  }
   renderSquare(x, y) {
-    return (
-      <button id="square" codeX={x} codeY={y}>
-        X
-      </button>
-    );
+    var {
+      showInfo,
+      xCoor,
+      yCoor,
+      isChess,
+      size,
+      chessCodeLetter,
+      chessCodeNumber,
+    } = this.state;
+    var run = x;
+    var rise = y;
+    var sizes = this.props.sizeValue;
+
+    if (xCoor == x && yCoor == y) {
+      console.log("amazing");
+      return (
+        <button
+          id="squareSelected"
+          codex={x}
+          codey={y}
+          onClick={() => this.showCode(run, rise, sizes)}
+        >
+          Q
+        </button>
+      );
+    } else
+      return (
+        <button
+          id="square"
+          codex={x}
+          codey={y}
+          onClick={() => this.showCode(run, rise, sizes)}
+        >
+          X
+        </button>
+      );
   }
 
   render() {
+    var {
+      showInfo,
+      xCoor,
+      yCoor,
+      isChess,
+      size,
+      chessCodeLetter,
+      chessCodeNumber,
+    } = this.state;
+    const viewSize = this.props.sizeValue;
+
     const elementS = [];
     const elementZ = [];
-    const viewSize = this.props.sizeValue;
 
     var x;
     var y;
+
+    var chessArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
     for (y = 0; y < viewSize; y++) {
       for (x = 0; x < viewSize; x++) {
-        elementS.push(<span>{this.renderSquare(x, y)}</span>);
+        elementS.push(<span>{this.renderSquare(x, viewSize - y - 1)}</span>);
       }
       elementZ.push(
         <div className="newLine">
@@ -34,8 +111,31 @@ class Building extends Component {
         elementS.pop();
       }
     }
+
+    const moreDisplay = (
+      <div>
+        {chessCodeLetter}
+        {chessCodeNumber}
+      </div>
+    );
+
+    const displayLocation = (
+      <div>
+        <p>
+          <span>{isChess ? moreDisplay : null}</span>( {xCoor} , {yCoor} )
+        </p>
+      </div>
+    );
+
+    const noneDisplay = (
+      <div>
+        <p>Click on a square for more info</p>
+      </div>
+    );
+
     return (
-      <div className="entireThing">
+      <div id="entireThing">
+        <div id="info">{showInfo ? displayLocation : noneDisplay}</div>
         <div>
           <span>
             {elementZ.map((value, index) => {
@@ -52,13 +152,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 1,
+      count: 0,
     };
   }
   enterCount() {
-    console.log("hello");
     var zvalue = document.getElementById("sizeHere").value;
-    console.log(zvalue);
+
     this.setState((state) => {
       return { count: zvalue };
     });
@@ -69,6 +168,7 @@ class App extends Component {
 
     const inputBox = (
       <div>
+        Eight Queens Puzzle
         <form>
           <input type="number" class="button" id="sizeHere"></input>
           <button
@@ -77,6 +177,13 @@ class App extends Component {
             onClick={() => this.enterCount()}
           >
             ENTER
+          </button>
+          <button
+            type="button"
+            class="button"
+            onClick={() => this.enterCount()}
+          >
+            Show Solutions
           </button>
         </form>
       </div>
